3472b296b0a1e451e43bc4a5f49149e3c6edfbd1
kusano <kusano@users.noreply.github.com>
2016-11-22 04:44:32 +0900

    THUMBNAILへの対応

--- worker.js
+++ worker.js
@@ -1,12 +1,14 @@
 import krkrsvchk from 'krkrsvchk';
 
 const reader = new FileReaderSync;
-const message = {};
-message[krkrsvchk.OK]           = '正常なセーブデータです。';
-message[krkrsvchk.BYTE_CODE]    = 'バイトコードファイルです。危険なTJSスクリプトが含まれている可能性があります。';
-message[krkrsvchk.FORMAT_ERROR] = 'ファイルの解析に失敗しました。セーブデータではないか、セーブデータがゲームの独自形式の可能性があります。';
-message[krkrsvchk.PARSE_ERROR]  = 'ファイルの解析に失敗しました。セーブデータに危険なTJSスクリプトが含まれている可能性があります。';
-message[krkrsvchk.MACRO]        = 'マクロが含まれています。マクロの中に危険なKAGスクリプトが含まれている可能性があります。';
+const message = {
+  [krkrsvchk.OK]:           '正常なセーブデータです。',
+  [krkrsvchk.BYTE_CODE]:    'バイトコードファイルです。危険なTJSスクリプトが含まれている可能性があります。',
+  [krkrsvchk.FORMAT_ERROR]: 'ファイルの解析に失敗しました。セーブデータではないか、セーブデータがゲームの独自形式の可能性があります。',
+  [krkrsvchk.PARSE_ERROR]:  'ファイルの解析に失敗しました。セーブデータに危険なTJSスクリプトが含まれている可能性があります。',
+  [krkrsvchk.MACRO]:        'マクロが含まれています。マクロの中に危険なKAGスクリプトが含まれている可能性があります。',
+  [krkrsvchk.THUMBNAIL]:    'サムネイルが含まれています。このツールはサムネイルを含んだセーブデータには対応していません。',
+};
 
 onmessage = event => {
   const files = event.data;
